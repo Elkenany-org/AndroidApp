10730056d17aeb3c45762db3037c3d6eade48b3a
PC-6 <hossam98.yasser@gmail.com>
2022-07-18 17:32:02 +0200

    finishing all the required adapter for the home sectore page , refactoring all the clickListeners to be in one generic clicklistner, refactoring all the item layout inflation to be customizable and reusable by only giving it a parameters and it will automatically generate the required layout

---
+++ app/src/main/java/com/example/elkenany/BindingAdapterUtil.kt
@@ -0,0 +1,35 @@
+package com.example.elkenany
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun ImageView.loadImage(url: String?) {
+    if (url!!.isEmpty()) {
+        this.visibility = View.GONE
+    } else {
+        Glide.with(this)
+            .load(url)
+            .centerCrop()
+            .into(this)
+    }
+}
+
+@BindingAdapter("itemText")
+fun TextView.writeText(text: String?) {
+    if (text!!.isEmpty()) {
+        this.visibility = View.GONE
+    } else {
+        this.text = text
+    }
+}
+
+//Generic ClickListener
+class ClickListener<T>(private val clickListener: (type: T) -> Unit) {
+    fun onClick(type: T) {
+        clickListener(type)
+    }
+}
--- app/src/main/java/com/example/elkenany/views/home/home_sector/HomeSectorFragment.kt
+++ app/src/main/java/com/example/elkenany/views/home/home_sector/HomeSectorFragment.kt
@@ -1,21 +1,94 @@
 package com.example.elkenany.views.home.home_sector
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.elkenany.ClickListener
 import com.example.elkenany.R
+import com.example.elkenany.databinding.FragmentHomeSectorBinding
+import com.example.elkenany.viewmodels.HomeSectorViewModel
+import com.example.elkenany.viewmodels.ViewModelFactory
+import com.example.elkenany.views.home.home_sector.adapter.*
 
 
 class HomeSectorFragment : Fragment() {
+    private lateinit var binding: FragmentHomeSectorBinding
+    private lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewModel: HomeSectorViewModel
+
+    // Recycler adapters
+    private lateinit var sectorsAdapter: SectorsAdapter
+    private lateinit var recommendationAdapter: SectorRecommendationAdapter
+    private lateinit var partnerAdapter: SectorsPartnerAdapter
+    private lateinit var stockAdapter: SectorsStockAdapter
+    private lateinit var guideAdapter: SectorsGuideAdapter
+    private lateinit var newsAdapter: SectorsNewAdapter
+    private lateinit var storeAdapter: SectorsStoreAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_sector, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_sector, container, false)
+        viewModelFactory = ViewModelFactory()
+        viewModel = ViewModelProvider(this, viewModelFactory)[HomeSectorViewModel::class.java]
+
+        //adapters initialization
+        sectorsAdapter = SectorsAdapter(ClickListener {
+            Toast.makeText(context, it.name.toString(), Toast.LENGTH_LONG).show()
+        })
+        recommendationAdapter = SectorRecommendationAdapter(ClickListener {
+            Toast.makeText(context, it.name.toString(), Toast.LENGTH_LONG).show()
+        })
+        partnerAdapter = SectorsPartnerAdapter(ClickListener { })
+        stockAdapter = SectorsStockAdapter(ClickListener { })
+        guideAdapter = SectorsGuideAdapter(ClickListener { })
+        newsAdapter = SectorsNewAdapter(ClickListener { })
+        storeAdapter = SectorsStoreAdapter(ClickListener { })
+
+        //assign each adapter to it's own recyclerView
+        binding.sectorsRecyclerView.adapter = sectorsAdapter
+        binding.recommendationRecyclerView.adapter = recommendationAdapter
+        binding.partnersRecyclerView.adapter = partnerAdapter
+        binding.stocksRecyclerView.adapter = stockAdapter
+        binding.guidesRecyclerView.adapter = guideAdapter
+        binding.newsRecyclerView.adapter = newsAdapter
+        binding.storesRecyclerView.adapter = storeAdapter
+
+        // Loading progressbar
+        viewModel.loading.observe(viewLifecycleOwner) {
+            if (it) {
+                binding.loadingProgressbar.visibility = View.VISIBLE
+                binding.line2.visibility = View.INVISIBLE
+            } else {
+                binding.loadingProgressbar.visibility = View.INVISIBLE
+                binding.line2.visibility = View.VISIBLE
+            }
+        }
+
+        //submitting all the recyclerAdapter lists to it value
+        viewModel.homeSectorsData.observe(viewLifecycleOwner) {
+            if (it != null) {
+                sectorsAdapter.submitList(it.sectors)
+                recommendationAdapter.submitList(it.sectorsRecomandtion)
+                partnerAdapter.submitList(it.sectorsLogos)
+                stockAdapter.submitList(it.sectorsStock)
+                guideAdapter.submitList(it.sectorsGuide)
+                newsAdapter.submitList(it.sectorsNews)
+                storeAdapter.submitList(it.sectorsStore)
+            } else {
+                Log.i("list", "Have no data to load")
+            }
+        }
+
+        return binding.root
     }
 
 }
