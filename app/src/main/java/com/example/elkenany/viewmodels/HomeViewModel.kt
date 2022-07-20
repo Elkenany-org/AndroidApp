7e5c33257ef92ab34b4d27a8bd647a314bcec740
PC-6 <hossam98.yasser@gmail.com>
2022-07-20 17:47:46 +0200

    fixing all navigation issues with current screens

--- app/src/main/java/com/example/elkenany/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/elkenany/viewmodels/HomeViewModel.kt
@@ -13,53 +13,11 @@ class HomeViewModel : ViewModel() {
     private val job = Job()
     private val _uiScope = CoroutineScope(Dispatchers.Main + job)
 
-    private val _homePage = MutableLiveData(false)
-    private val _searchPage = MutableLiveData(false)
-    private val _stockPage = MutableLiveData(false)
-    private val _profilePage = MutableLiveData(false)
-
-    val homePage: LiveData<Boolean> get() = _homePage
-    val searchPage: LiveData<Boolean> get() = _searchPage
-    val stockPage: LiveData<Boolean> get() = _stockPage
-    val profilePage: LiveData<Boolean> get() = _profilePage
-
-    init {
-        _homePage.value = true
-    }
-
     override fun onCleared() {
         super.onCleared()
         job.cancel()
     }
 
-    fun onDoneNavigating() {
-
-        _homePage.value = false
-        _profilePage.value = false
-        _searchPage.value = false
-        _stockPage.value = false
-    }
 
 
-    fun onItemClicked(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.homePage -> {
-                _homePage.value = true
-                true
-            }
-            R.id.searchPage -> {
-                _searchPage.value = true
-                true
-            }
-            R.id.stockPage -> {
-                _stockPage.value = true
-                true
-            }
-            R.id.profilePage -> {
-                _profilePage.value = true
-                true
-            }
-            else -> false
-        }
-    }
 }
